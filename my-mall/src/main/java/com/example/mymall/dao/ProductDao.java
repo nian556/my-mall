d7fc171efd027499a8f259e7caab9fab4f611f43
nian556 <o199777o@gmail.com>
2024-05-08 09:26:36 +0800

    商品功能 - 實作查詢商品列表 基礎實作

--- my-mall/src/main/java/com/example/mymall/dao/ProductDao.java
+++ my-mall/src/main/java/com/example/mymall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.example.mymall.dao;
 import com.example.mymall.dto.ProductResquest;
 import com.example.mymall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- my-mall/src/main/java/com/example/mymall/service/ProductService.java
+++ my-mall/src/main/java/com/example/mymall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.example.mymall.service;
 import com.example.mymall.dto.ProductResquest;
 import com.example.mymall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
