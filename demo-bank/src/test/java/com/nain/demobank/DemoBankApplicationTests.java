d4ea27d21d12c3a5451659fba8dab6ee986391c8
nian556 <o199777o@gmail.com>
2024-05-21 21:11:03 +0800

    初始化Spring Boot project

---
+++ demo-bank/src/test/java/com/nain/demobank/DemoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nain.demobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
